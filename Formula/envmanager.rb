7be46516c495d9d05732afb4f3e72899b82ddd99
Patrick Gerrits <info@gecab.nl>
2026-06-19 09:59:39 +0200

    Update envmanager to 0.5.0

--- Formula/envmanager.rb
+++ Formula/envmanager.rb
@@ -1,8 +1,8 @@
 class Envmanager < Formula
   desc "CLI for EnvManager - secure environment variable management"
   homepage "https://envmanager.dev"
-  url "https://registry.npmjs.org/@envmanager-cli/cli/-/cli-0.0.0.tgz"
-  sha256 "PLACEHOLDER"
+  url "https://registry.npmjs.org/@envmanager-cli/cli/-/cli-0.5.0.tgz"
+  sha256 "f23490fba308cb1dab12c3b95679c245709bcc2f4e0f57173ffb42fafd592595"
   license "SEE LICENSE IN LICENSE"
 
   depends_on "node"
